b42028160b2ba124e8c022178f4aa96264fa2119
Muhammad Junaid Khan <48877067+jd00738@users.noreply.github.com>
2020-01-20 11:54:07 +0500

    Bugs Fixed New Entity introduce Include function Ex 1 done

--- MoviesOnDemand/Controllers/CustomersController.cs
+++ MoviesOnDemand/Controllers/CustomersController.cs
@@ -5,54 +5,79 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Data.Entity;
 
 namespace MoviesOnDemand.Controllers
 {
     public class CustomersController : Controller
     {
-        // GET: Customers
-        public ActionResult Index()
+        private ApplicationDbContext _context;
+
+        public CustomersController()
         {
-            var customers = new List<Customer>
-            {
-                new Customer{Name="Muhammad Junaid Khan",Id=1},
-                new Customer{Name="Sardar Mudassar",Id=2}
-            };
+            _context = new ApplicationDbContext();
+        }
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
+
+        // GET: Customers
+        public ViewResult Index()
+        {
+            var customers = _context.Customers.Include(c=>c.MembershipType).ToList();
+            //return View(customers);
+            //var customers = new List<Customer>
+            //{
+            //    new Customer{Name="Muhammad Junaid Khan",Id=1},
+            //    new Customer{Name="Sardar Mudassar",Id=2}
+            //};
             var ViewModel = new RandomMovieViewModel()
             {
                 Customers = customers
             };
             return View(ViewModel);
         }
+
         public ActionResult Edit(int id)
         {
-#pragma warning disable CS0472 // The result of the expression is always the same since a value of this type is never equal to 'null'
-            if (id == null)
-#pragma warning restore CS0472 // The result of the expression is always the same since a value of this type is never equal to 'null'
+            var customer = _context.Customers.Include(c=>c.MembershipType).SingleOrDefault(c => c.Id == id);
+            var cust = new List<Customer>() { new Customer
+                {
+                    Name = customer.Name,
+                    Id = customer.Id,
+                    IsSubcribeToNewsLetter = customer.IsSubcribeToNewsLetter,
+                    MembershipType = customer.MembershipType
+                }
+                };
+            var ViewModel = new RandomMovieViewModel();
+            //#pragma warning disable CS0472 // The result of the expression is always the same since a value of this type is never equal to 'null'
+            //if (id == null)
+            //#pragma warning restore CS0472 // The result of the expression is always the same since a value of this type is never equal to 'null'
+            if (customer == null)
             {
                 return HttpNotFound();
-
             }
             else
-            {
-                var customers = new List<Customer>
-            {
-                new Customer{Name="Muhammad Junaid Khan",Id=1},
-                new Customer{Name="Sardar Mudassar",Id=2}
-            };
-                var aj = "";
-                foreach (var item in customers)
-                {
-                    if (item.Id == id)
-                    {
-                        aj = item.Name;
-                    }
-                }
-                var cust = new List<Customer>() { new Customer { Name = aj } };
-                var ViewModel = new RandomMovieViewModel() { Customers = cust };
-                return View(ViewModel);
-            }
-            
+                //{
+                //    var customers = new List<Customer>
+                //{
+                //    new Customer{Name="Muhammad Junaid Khan",Id=1},
+                //    new Customer{Name="Sardar Mudassar",Id=2}
+                //};
+                //    var aj = "";
+                //    foreach (var item in customers)
+                //    {
+                //        if (item.Id == id)
+                //        {
+                //            aj = item.Name;
+                //        }
+                //    }
+
+                ViewModel.Customers = cust;
+              return View(ViewModel);
         }
+
     }
 }
--- MoviesOnDemand/Migrations/Configuration.cs
+++ MoviesOnDemand/Migrations/Configuration.cs
@@ -9,7 +9,7 @@ namespace MoviesOnDemand.Migrations
     {
         public Configuration()
         {
-            AutomaticMigrationsEnabled = false;
+            AutomaticMigrationsEnabled = true;
         }
 
         protected override void Seed(MoviesOnDemand.Models.ApplicationDbContext context)
